862e165348513daed8f5a4bfdc007c96eb0c49b8
moriroKim <moriroKim@gmail.com>
2024-07-30 23:04:57 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Detail from './routes/Detail';
 function App() {
     return (
         <Routes>
-            <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />}></Route>
-            <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+            <Route path="/movie/:id" element={<Detail />}></Route>
+            <Route path="/" element={<Home />}></Route>
         </Routes>
     );
 }
